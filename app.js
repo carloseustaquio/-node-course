994b349b71855b0df563dcad13d3729cca5da9d0
Carlos Eustáquio <caeu227@gmail.com>
2019-12-19 16:09:05 -0300

    Start adding controllers

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = express()
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
-const adminData = require('./routes/admin')
+const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded())
 
-app.use('/admin', adminData.routes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
 app.use(express.static(path.join(__dirname, 'public')))
