76f5aafadf7c739daf18e5316475a1160d3a07ae
Carlos Eustáquio <caeu227@gmail.com>
2019-12-19 15:14:51 -0300

    Inicia projeto

---
+++ app.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const routeDir = require('./util/path')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded())
+
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page not Found', path: '' })
+})
+
+app.listen(3000)
