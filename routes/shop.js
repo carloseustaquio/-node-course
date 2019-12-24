e96ef29e21940d915733e9d66e75635256f83b88
Carlos Eustáquio <caeu227@gmail.com>
2019-12-24 19:49:42 -0300

    Cria novas rotas e integra com controllers.

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,17 @@
-// const path = require('path')
+const express = require("express");
 
-const express = require('express')
+const shopController = require("../controllers/shop");
 
-const productsController = require('../controllers/products')
+const router = express.Router();
 
-const router = express.Router()
+router.get("/", shopController.getIndex);
 
-router.get('/', productsController.getProducts)
+router.get("/products", shopController.getProducts);
 
-module.exports = router
+router.get("/cart", shopController.getCart);
+
+router.get("/orders", shopController.getOrders);
+
+router.get("/checkout", shopController.getCheckout);
+
+module.exports = router;
