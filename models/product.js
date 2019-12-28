44a7076e44494d17898f48b16ef512277ba64987
Carlos Eustáquio <caeu227@gmail.com>
2019-12-27 22:54:33 -0300

    Testa conexão com banco de dados utilizando pacote node mysql2

--- models/product.js
+++ models/product.js
@@ -1,19 +1,6 @@
-const fs = require("fs");
-const path = require("path");
-const dirPath = require("../util/path");
+const db = require("../util/database");
+
 const Cart = require("./cart");
-
-const p = path.join(dirPath, "data", "products.json");
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      return cb([]);
-    } else {
-      return cb(JSON.parse(fileContent));
-    }
-  });
-};
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -25,49 +12,19 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(
-          prod => prod.id === this.id
-        );
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
+    return db.execute(
+      "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static delete(id) {
-    getProductsFromFile(products => {
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err) {
-          const productPrice = products.find(prod => prod.id === id).price;
-          console.log(productPrice);
-          Cart.deleteProduct(id, productPrice);
-        } else {
-          console.log(err);
-        }
-      });
-    });
+  static delete(id) {}
+
+  static fetchAll() {
+    return db.execute("SELECT * FROM products");
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
-
-  static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id);
-      cb(product);
-    });
+  static findById(id) {
+    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
   }
 };
