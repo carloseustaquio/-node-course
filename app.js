6cf1bc59a3355a487c540dae9e40d3b5ec80f807
Carlos Eustáquio <caeu227@gmail.com>
2019-12-31 00:19:32 -0300

    Adiciona relacionamentos entre classes do sequelize em app.js

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -14,12 +20,49 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      console.log(req.user);
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, {
+  constraints: true,
+  onDelete: "CASCADE"
+});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem }); // Em um carrinho, pode haver vários produtos.
+Product.belongsToMany(Cart, { through: CartItem }); // Um produto pode estar em vários carrinhos.
+
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(res => {
+    return User.findByPk(1);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: "Carlos", email: "carlos@teste.com" });
+    }
+    return user;
+  })
+  .then(user => {
+    console.log(user);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
