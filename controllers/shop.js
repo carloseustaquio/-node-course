68dedd95fb0b3face2fc8c994a3371ed5a95ec00
Carlos Eustáquio <caeu227@gmail.com>
2020-01-01 15:09:40 -0300

    Atualiza controllers (para sequelize) getCart, postCart e postCartDeleteProduct.

--- controllers/shop.js
+++ controllers/shop.js
@@ -48,41 +48,80 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll(products => {
-      const cartProducts = [];
-      for (let product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-
-      res.render("shop/cart", {
-        pageTitle: "Your Cart",
-        path: "/cart",
-        products: cartProducts
-      });
-    });
-  });
+  console.log(req.user.cart);
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart
+        .getProducts()
+        .then(products => {
+          res.render("shop/cart", {
+            pageTitle: "Your Cart",
+            path: "/cart",
+            products: products
+          });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
+  // adiciona produto ao carrinho
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect("/");
+  let fetchedCart; // armazena carrinho selecionado
+  let newQuantity = 1; // armazena quantidade do produto a ser inserida no carrinho.
+  req.user
+    .getCart()
+    .then(cart => {
+      // salva carrinho e chama produtos do carrinho
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      // recebe array com produto selecionado
+      let product;
+      if (products.length > 0) {
+        // se array contiver algum produto, salva em variavel product
+        product = products[0];
+      }
+      if (product) {
+        // se houver product, atualiza sua quantidade
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId); // se não houver product, simplesmente retorna produto (após busca-lo em tabela products, caso contrário, retornaria undefined)
+    })
+    .then(product => {
+      // adiciona produto ao carrinho
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect("/cart");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getOrders = (req, res, next) => {
